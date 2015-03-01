48ca842bda2092f577207f84a5e8355989d7f02f
KatWenger <katherinewenger@gmail.com>
2015-03-01 10:35:17 -0500

    Done

--- email_parser.rb
+++ email_parser.rb
@@ -1,6 +1,14 @@
 # Create a class EmailParser with a parse method that accepts a string of unformatted email addresses as an argument. The parse method should separate the string into an array of unique email addresses (removing any duplicates). The delimiters to support are commas (','), spaces (' ') and semicolons (';'). 
+class EmailParser
+	def parse(emails)
+		emails.split(/\s|,\s|;\s/).uniq
+
+	end
+end
+	
+		
 
 
 
 # Bonus!
-# Create a validate method that tests if the email address is potentially valid (not necessarily that it is actually real).
+# Create a validate method that tests if the email address is potentially valid (not necessarily that it is actually real)
